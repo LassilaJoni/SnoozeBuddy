87ec9eb68f1edfdd6491d52f099706e6e546b598
edvni <104062136+edvni@users.noreply.github.com>
2022-04-26 15:57:55 +0200

    Added Sleep start time button in MainActivity

--- app/src/main/java/fi/jonilassila/snoozebuddy/MainActivity.java
+++ app/src/main/java/fi/jonilassila/snoozebuddy/MainActivity.java
@@ -2,13 +2,50 @@ package fi.jonilassila.snoozebuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TimePicker;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sleepStartButton;
+    int hour, minute;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sleepStartButton = findViewById(R.id.sleepStartTime);
 
+    }
+
+
+    /** Time picker to set sleeping start time
+     *
+     * @param view
+     * Done with the help of YouTube content creator Code With Cal
+     */
+    public void popTimePicker(View view) {
+
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                hour = selectedHour;
+                minute = selectedMinute;
+                sleepStartButton.setText(String.format(Locale.getDefault(),"%02d:%02d", hour, minute));
+
+            }
+        };
+
+        int style = AlertDialog.THEME_HOLO_DARK;
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, style, onTimeSetListener, hour, minute, true);
+        timePickerDialog.setTitle("Select time");
+        timePickerDialog.show();
     }
 }
