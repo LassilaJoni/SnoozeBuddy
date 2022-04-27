1d1d16acbbdfd92a4c7e76d081555b8cd80d854c
LassilaJoni <joni@jonilassila.com>
2022-04-27 17:10:09 +0300

    "Working version"

--- app/src/main/java/fi/jonilassila/snoozebuddy/ListDataActivity.java
+++ app/src/main/java/fi/jonilassila/snoozebuddy/ListDataActivity.java
@@ -1,10 +1,7 @@
 package fi.jonilassila.snoozebuddy;
 
-import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -14,11 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
-
-
 public class ListDataActivity extends AppCompatActivity {
     private static final String TAG = "ListDataActivity";
-    public final static String EXTRA = "fi.jonilassila.snoozebuddy_EXTRA";
+
     DatabaseHelper databaseHelper;
 
     private ListView listView;
@@ -27,23 +22,11 @@ public class ListDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_layout);
+        listView = (ListView) findViewById(R.id.listview);
         databaseHelper = new DatabaseHelper(this);
-        ListView lv = findViewById(R.id.listview);
+
         populateListView();
-
-
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent nextActivity = new Intent(ListDataActivity.this, DetailsActivity.class);
-                nextActivity.putExtra(EXTRA, i);
-                startActivity(nextActivity);
-
-            }
-        });
-
     }
-
 
     private void populateListView() {
         Cursor data = databaseHelper.getData();
@@ -55,6 +38,5 @@ public class ListDataActivity extends AppCompatActivity {
         ListAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
         listView.setAdapter(adapter);
     }
-
 
 }
